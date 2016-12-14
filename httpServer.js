ac0b9a51ad04c31f38ccd2b57dedf71ed7a27c8b
Jamieson Bates <jamiesonbates@gmail.com>
2016-12-13 16:24:29 -0800

    Add bonus 1 solution

--- httpServer.js
+++ httpServer.js
@@ -8,58 +8,52 @@ const path = require('path');
 
 const petsPath = path.join(__dirname, 'pets.json');
 
-const errGuard = function(err) {
-  if (err) {
-    console.error(err.stack);
-
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Internal server error');
-
-    return;
-  }
-}
+const petRegExp = /^\/pets\/(.*)$/;
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/pets') {
+  if (req.method === 'GET' && req.url === petRegExp) {
     fs.readFile(petsPath, 'utf8', (err, data) => {
-      errGuard(err);
+      if (err) {
+        console.error(err.stack);
 
-      res.setHeader('Content-Type', 'application/json');
-      res.end(data);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal server error');
+
+        return;
+      }
+
+      if (req.url === '/pets/ ') {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+      else if (req.url === '/pets/1') {
+        const pets = JSON.parse(data);
+        const petJSON = JSON.stringify(pets[0]);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(petJSON);
+      }
+      else if (req.url === '/pets/2') {
+        const pets = JSON.parse(data);
+        const petJSON = JSON.stringify(pets[1]);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(petJSON);
+      }
+      else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
+      }
     });
-  }
-  else if (req.method === 'GET' && req.url === '/pets/1') {
-    fs.readFile(petsPath, 'utf8', (err, data) => {
-      errGuard(err);
-
-      const pets = JSON.parse(data);
-      const petJSON = JSON.stringify(guests[0]);
-
-      res.setHeader('Content-Type', 'application/json');
-      res.end(petJSON);
-    });
-  }
-  else if (req.method === 'GET' && req.url === '/pets/2') {
-    fs.readFile(petsPath, 'utf8', (err, data) => {
-      errGuard(err);
-
-      const pets = JSON.parse(data);
-      const petJSON = JSON.stringify(pets[1]);
-
-      res.setHeader('Content-Type', 'application/json');
-      res.end(petJSON);
-    });
-  }
-  else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found');
   }
 });
 
-const port = 8000;
+const port = 7500;
 
 server.listen(port, () => {
   console.log(`Listening from port ${port}`);
 });
+
+module.exports = server;
