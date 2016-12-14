436ac82af439605a80772f9e16ff9fcb68bac1a3
Jamieson Bates <jamiesonbates@gmail.com>
2016-12-14 15:50:26 -0800

    Add Express server that can handle GET requests

---
+++ expressServer.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+app.get('/pets', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      res.sendStatus(500);
+    }
+
+    const pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+
+app.get('/pets/:id', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      res.sendStatus(500);
+    }
+
+    const id = Number.parseInt(req.params.id);
+    const pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.send(pets[id]);
+  });
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening from port ${port}`);
+});
+
+module.exports = app;
