c8b6bbbe3bb4de7dd9b050711669b46c825d658f
Jamieson Bates <jamiesonbates@gmail.com>
2016-12-15 13:46:59 -0800

    Refactor Express server to be RESTful

--- expressServer.js
+++ expressServer.js
@@ -19,6 +19,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+// Read All
 app.get('/pets', (req, res) => {
   fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
     if (readErr) {
@@ -32,6 +33,7 @@ app.get('/pets', (req, res) => {
   });
 });
 
+// Post One
 app.post('/pets', (req, res) => {
   fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
     if (readErr) {
@@ -69,6 +71,7 @@ app.post('/pets', (req, res) => {
   });
 });
 
+// Read One
 app.get('/pets/:id', (req, res) => {
   fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
     if (err) {
@@ -85,6 +88,85 @@ app.get('/pets/:id', (req, res) => {
 
     res.set('Content-Type', 'application/json');
     res.send(pets[id]);
+  });
+});
+
+// Update One Dynamically
+app.patch('/pets/:id', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      console.error(readErr.stack);
+      res.sendStatus(500);
+    }
+
+    const id = Number.parseInt(req.params.id);
+    const pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id > pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    const age = req.body.age;
+    const kind = req.body.kind;
+    const name = req.body.name;
+
+    if (!age && !kind && !name) {
+      return res.sendStatus(400);
+    }
+
+    if (age) {
+      pets[id].age = age;
+    }
+
+    if (kind) {
+      pets[id].kind = kind;
+    }
+
+    if (name) {
+      pets[id].name = name;
+    }
+
+    const newPetsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, newPetsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'application/json');
+      res.send(pets[id]);
+    });
+  });
+});
+
+// Destroy One
+app.delete('/pets/:id', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      console.error(readErr.stack);
+      res.sendStatus(500);
+    }
+
+    const pets = JSON.parse(petsJSON);
+    const id = Number.parseInt(req.params.id);
+
+    if (id < 0 || id > pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    const pet = pets.splice(id, 1)[0];
+    const newPetsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, newPetsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'application/json');
+      res.send(pet);
+    });
   });
 });
 
